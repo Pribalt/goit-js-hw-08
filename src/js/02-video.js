c4c3e18f8ac2d08f0e479514b48df14cf14c592f
Pribalt <vitalisvitalik@gmail.com>
2022-12-26 01:23:51 +0200

    change_02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-console.log(throttle);
-console.log(Player);
+
+const iframeIdEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeIdEl);
+const KEY_PLAYBACK_TIME = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  const seconds = JSON.stringify(data.seconds);
+  localStorage.setItem(KEY_PLAYBACK_TIME, seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const getLocal = JSON.parse(localStorage.getItem(KEY_PLAYBACK_TIME) || 0);
+
+player.setCurrentTime(getLocal);
