bf1c7c880845e0d05d3d169bd2a214ebff846263
Pribalt <vitalisvitalik@gmail.com>
2022-12-25 18:45:49 +0200

    change_01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,4 +4,31 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(SimpleLightbox);
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+         <a class="gallery__item"
+         href="${original}">
+         <img class="gallery__image"
+         src="${preview}"
+         alt="${description}"
+         />
+         </a>
+         `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
